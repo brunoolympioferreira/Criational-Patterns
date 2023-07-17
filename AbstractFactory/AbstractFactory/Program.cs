bf32782eea2ad22243d6b6d120c2a604e6fee7d8
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2023-07-17 12:20:51 -0300

    Abstract Method Pattern parte 1

---
+++ AbstractFactory/AbstractFactory/Program.cs
@@ -0,0 +1,21 @@
+﻿using AbstractFactory1;
+
+IMontadora fabrica = new FabricaHonda();
+ISedan sedan = fabrica.CriarSedan();
+ISuv suv = fabrica.CriarSuv();
+Console.WriteLine("********* HONDA ********");
+ExibeDetalhes(sedan, suv);
+
+fabrica = new FabricaHyundai();
+sedan = fabrica.CriarSedan();
+suv = fabrica.CriarSuv();
+Console.WriteLine("******* HYUNDAI **********");
+ExibeDetalhes(sedan, suv);
+
+Console.ReadKey();
+
+static void ExibeDetalhes(ISedan sedan, ISuv suv)
+{
+    Console.WriteLine(sedan.ExibeDetalhes());
+    Console.WriteLine(suv.ExibeDetalhes());
+}
