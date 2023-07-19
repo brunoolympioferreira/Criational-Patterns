9c6cc3d8c1aa48860550242abfce579c56576281
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2023-07-19 15:58:08 -0300

    Builder Pattern

---
+++ Builder/Builder/Program.cs
@@ -0,0 +1,14 @@
+﻿//instancia de Director
+using Builder_Computer;
+
+Fabricante fabricante = new Fabricante();
+
+//instancia do concretebuilder
+NotebookBuilder notebookBuilder = new NotebookBuilder();
+DesktopBuilder desktopBuilder = new DesktopBuilder();
+
+//cria objetos conforme o builder
+fabricante.Build(notebookBuilder);
+fabricante.Build(desktopBuilder);
+
+Console.ReadLine();
