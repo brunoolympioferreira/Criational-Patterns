0c1063172fce3d8298570eb53b2969b8be0fcd4b
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2023-07-31 18:52:13 -0300

    Prototype Pattern

---
+++ Prototype/Exemplo1/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+//Cria objeto do tipo Soldado
+using Exemplo1;
+
+Soldado soldado = new Soldado();
+soldado.Nome = "Soldado";
+soldado.Arma = "Fuzil";
+
+Soldado soldado_clone1 = (Soldado)soldado.Clone();
+Soldado soldado_clone2 = (Soldado)soldado.Clone();
+
+// atribui valores ao clone1
+soldado_clone1.Nome = "Soldado Clone1";
+soldado_clone1.Arma = "Bazuca";
+
+// atribui valores ao clone2
+soldado_clone2.Nome = "Soldado Clone2";
+soldado_clone2.Arma = "Metralhadora";
+
+//exibe valores do objeto original
+WriteLine(">> Objeto Original");
+WriteLine($"{soldado.Nome} - {soldado.Arma}");
+
+//Valores de objeto clone1
+WriteLine(">> Objeto Clone1");
+WriteLine($"{soldado_clone1.Nome} -  {soldado_clone1.Arma}");
+
+//Valores de objeto clone2
+Console.WriteLine(">> Objeto Clone2");
+WriteLine($"{soldado_clone2.Nome} - {soldado_clone2.Arma}");
+
+
+ReadLine();
---
+++ Prototype/Exemplo3/Program.cs
@@ -0,0 +1,27 @@
+﻿using Exercicio1;
+
+Programador programador = new Programador();
+programador.Nome = "Jefferson";
+programador.Cargo = "Programador Pleno";
+programador.Linguagem = "C#";
+
+Programador programadorClone = (Programador)programador.Clone();
+programadorClone.Nome = "Sueli";
+programadorClone.Cargo = "Programador Junior";
+
+Console.WriteLine(programador);
+Console.WriteLine(programadorClone);
+
+Engenheiro engenheiro = new Engenheiro();
+engenheiro.Nome = "Mario";
+engenheiro.Cargo = "Engenheiro Mecânico";
+engenheiro.Especialidade = "Motores";
+
+Engenheiro engenheiroClone = (Engenheiro)engenheiro.Clone();
+engenheiroClone.Nome = "Amanda";
+engenheiroClone.Especialidade = "Fluidos";
+
+Console.WriteLine(engenheiro);
+Console.WriteLine(engenheiroClone);
+
+Console.ReadLine();
